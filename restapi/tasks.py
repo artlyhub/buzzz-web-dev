5bcdc55857111dd14ce355386a170b9fed87f2d9
artlyhub <ppark9553@gmail.com>
2017-12-03 20:17:41 +0900

    test tasks again....

--- restapi/tasks.py
+++ restapi/tasks.py
@@ -1,24 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
-from datetime import datetime
-from selenium import webdriver
-import pyautogui
-import os
-from bs4 import BeautifulSoup
-import requests
-import sqlite3
-import time
-import simplejson as json
 from celery.decorators import task
-
-from restapi.models import Ticker, OHLCV
-
-
 
 @task(name="scrape_naver_ohlcv")
 def hi():
-	print('hi hi')
+	return 'hi'
 
 @task(name="test")
 def test():
-	print('hi test hi')
+	return 'hi'
