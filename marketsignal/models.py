31bc701f4c3032ecc4b93ac85276312f36aeddea
artlyhub <ppark9553@gmail.com>
2017-12-05 20:49:39 +0900

    market signal model

--- marketsignal/models.py
+++ marketsignal/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+SIGNAL = (
+    ('B', 'Buy'),
+    ('S', 'Sell'),
+    ('H', 'Hold'),
+)
+
+RATING = (
+    'A',
+    'B',
+    'C',
+)
+
+
+class KOSPI(models.Model):
+    date = models.CharField(max_length=8)
+    index = models.FloatField()
+    index_change = models.FloatField()
+    pct_change = models.FloatField()
+    volume = models.BigIntegerField()
+    signal = models.CharField(max_length=1,
+                              choices=SIGNAL,
+                              blank=True,
+                              null=True)
+    signal_days = models.IntegerField()
+    rating = models.CharField(max_length=1,
+                              choices=RATING,
+                              blank=True,
+                              null=True)
+
+
+class KOSDAQ(models.Model):
+    date = models.CharField(max_length=8)
+    index = models.FloatField()
+    index_change = models.FloatField()
+    pct_change = models.FloatField()
+    volume = models.BigIntegerField()
+    signal = models.CharField(max_length=1,
+                              choices=SIGNAL,
+                              blank=True,
+                              null=True)
+    signal_days = models.IntegerField()
+    rating = models.CharField(max_length=1,
+                              choices=RATING,
+                              blank=True,
+                              null=True)
