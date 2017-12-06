2de9db6873efe4f4b5f9aa5a3cc4c566c50b9d67
yorong <tpgml6824@gmail.com>
2017-12-06 16:06:08 +0900

    test everyday ohlcv

--- buzzz/celery.py
+++ buzzz/celery.py
@@ -41,12 +41,12 @@ app.conf.beat_schedule = {
 
     'scrape-naver-ohlcv-at-4': {
     	'task': 'scrape_naver_ohlcv',
-    	'schedule': crontab(hour=16),
+    	'schedule': crontab(hour=16, minute=10),
         'args': (),
     },
     'scrape-naver-info-at-4:30': {
         'task': 'scrape_naver_info',
-        'schedule': crontab(hour=16, minute=30),
+        'schedule': crontab(hour=17),
         'args': ()
     }
     
